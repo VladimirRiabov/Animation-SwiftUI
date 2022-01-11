c5bdbe864c05ada0454970774ad8173471d7640d
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-11 20:08:17 +0300

    Initial Commit

---
+++ Animation SwiftUI/Animation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_SwiftUIApp.swift
+//  Animation SwiftUI
+//
+//  Created by Владимир Рябов on 11.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Animation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
